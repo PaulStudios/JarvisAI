9a21c07823e2f7e72abe58930ab5a94c87071832
HilFing <indradip.paul@outlook.com>
2022-12-22 19:32:20 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -8,14 +8,16 @@ import basicfuncs
 
 class TestClass:
     """Test class for main.py"""
-    import basicfuncs
     def test_server_connection(self):
+        """Testing if API if server is online"""
         assert basicfuncs.CONNECTION == 1
 
     def test_login(self):
+        """Testing login module"""
         assert basicfuncs.login_back("test", "tester") == "success"
         assert "test" == basicfuncs.USER
 
     def test_bot(self):
+        """testing bot response module"""
         assert basicfuncs.CHATBOT.authorname == "HilFing"
         assert basicfuncs.talk("Hi") == "Hi there!"
