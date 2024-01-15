5fc5d03170b7df27ee75a7d6d66658daaa4cc2fa
HilFing <81067543+hilfing@users.noreply.github.com>
2024-01-15 23:07:37 +0530

    Fix

--- test_main.py
+++ test_main.py
@@ -4,27 +4,9 @@
 Pytest test file
 """
 
-# import basicfuncs
-
-
 class TestClass:
     """Test class for gui.py"""
 
     def test_temp(self):
         """Temporary"""
         assert 1 + 1 == 2
-
-
-#    def test_server_connection(self):
-#        """Testing if API server is online"""
-#        assert basicfuncs.CONNECTION == 1
-
-#    def test_login(self):
-#        """Testing login module"""
-#        assert basicfuncs.login_back("test", "tester") == "success"
-#        assert "test" == basicfuncs.USER
-
-#    def test_bot(self):
-#        """testing bot response module"""
-#        assert basicfuncs.CHATBOT.authorname == "HilFing"
-#        assert basicfuncs.talk("Hi") == "Hi there!"
